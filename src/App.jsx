4c420650275fb8208263ffb614cbee12a90bd3de
Ashutosh chauhan <chauhanashutosh287@gmail.com>
2025-04-03 13:00:10 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import Home from './Home'
+import Book from './pages/Book'
+import View from './pages/View'
+import { NavLink, Route, Routes } from 'react-router-dom'
 
 export default function App() {
   return (
-    <div>
-      <h1>hiii</h1>
-    </div>
+   <>
+   <nav>
+   <div className="content">
+    {/* <img src="" alt="" /> */}
+    <ul>
+        <li> <NavLink to="/" >Home</NavLink></li>
+        <li> <NavLink to="/book" >Book Appointment</NavLink></li>
+        <li> <NavLink to="/view" >View list</NavLink></li>
+    </ul>
+
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/book" element={<Book />} />
+        <Route path="/view" element={<View />} />
+      </Routes>
+
+
+   </div>
+   </nav>
+   </>
   )
 }
